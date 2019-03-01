0670584879c76fc1831efb7569d01e0d7edfa69a
Alexandra <a.r.chumachenko@gmail.com>
2019-03-02 00:18:49 +0200

    Last edit

--- LaboratoryCSharp_2/LaboratoryCSharp_2/ILoaderOwner.cs
+++ LaboratoryCSharp_2/LaboratoryCSharp_2/ILoaderOwner.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
 
 namespace LaboratoryCSharp_2
@@ -14,3 +9,4 @@ namespace LaboratoryCSharp_2
         bool IsControlEnabled { get; set; }
     }
 }
+
